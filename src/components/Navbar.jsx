8018018819fb650dac442dc50935411a350a72ca
IdanHarari11 <idanharari112@gmail.com>
2024-09-15 17:33:53 +0300

    chore: Update npm dependencies and add Prettier code formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,7 +62,7 @@ export default function Navbar() {
   );
 
   return (
-    <div className="sticky top-0 z-10 bg-current flex justify-between items-center p-2" dir="rtl">
+    <div className="sticky top-0 z-10 bg-current flex justify-between items-center p-2" style={{backgroundColor: 'rgba(245, 246, 245, 66%)'}} dir="rtl">
       <div className="flex-shrink-0 order-2">
         {start}
       </div>
